d0e829d8a7074565b003b4607fc7cd29fbe2c935
gvelizf <86574409+GenesisAVelizf@users.noreply.github.com>
2022-02-22 21:18:07 -0500

    Add files via upload

---
+++ PROYECTO_MENÚ/cola.py
@@ -0,0 +1,32 @@
+class Cola:
+    
+     
+     cola =["Sofia","Karen","Julian","Andres"]
+     
+     #Agregamos elementos al final de la cola
+     cola.append("Ana")
+     cola.append("Tomas")
+     
+     print(cola)
+     
+     #Sacando elementos por el principio de la cola
+     
+     n = cola.pop(0)
+     print(f"Atendiendo a {n}")
+     
+     n = cola.pop(0)
+     print(f"Atendiendo a {n}")
+     
+     n = cola.pop(0)
+     print(f"Atendiendo a {n}")
+     
+     n = cola.pop(0)
+     print(f"Atendiendo a {n}")
+     
+     n = cola.pop(0)
+     print(f"Atendiendo a {n}")
+     
+     n = cola.pop(0)
+     print(f"Atendiendo a {n}")
+     
+     print(cola)
---
+++ PROYECTO_MENÚ/pilas.py
@@ -0,0 +1,78 @@
+class Stack:                
+    def __init__(self,tamanio):
+        self.lista=[]
+        self.tope=0
+        self.size=tamanio
+     
+    def empty(self):
+        # if self.tope == 0:
+        #     return True
+        # else:
+        #     return False
+        return self.tope == 0
+    
+    def push(self,dato):
+        if self.tope < self.size:
+            self.lista += [dato]
+            self.tope += 1
+        else:
+            print("La Pila está Llena")
+   
+    def pop(self):
+        if self.empty():
+            return "Lista Vacia"
+        else:
+            top = self.lista[-1]
+            self.tope -= 1
+            self.lista = self.lista[:-1]
+            return top
+            
+    def longitud(self):
+        return self.tope
+        
+    def show(self):
+        if self.empty():
+            print("Lista vacia")
+        else:                    
+            for tope in range(self.tope-1,-1,-1):
+                print("[{}]".format(self.lista[tope]))    
+    
+    def buscar(self,buscado):
+        return "Retorna la posicion del valor buscado"
+   
+pila = Stack(5)
+pila.push("4")       
+pila.push("3")       
+pila.push("2")       
+pila.push("5")       
+pila.push("20")       
+pila.push("10")   
+pila.show()    
+print(pila.pop())
+print(pila.pop())
+print(pila.pop())
+print(pila.pop())
+print(pila.pop())
+print(pila.pop())
+
+# Menu
+# 1) Lista
+#     1) push    ingres numero(4)
+#     2) pop
+#     3) show
+#     4) eliminar ingrese la posicon a eliminar(2)
+#     5) insertar
+#     6) buscar
+# 2) pilas
+#     1) push
+#     2) pop
+#     3) show
+#     4) buscar
+#     5) longitud
+
+# 3) colas
+#     1) push
+#     2) pop
+#     3) show
+#     4) buscar
+#     5) longitud
